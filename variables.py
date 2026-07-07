bddb58adeabfc21553c8d561fda75e655d710057
Nandam Bharani <nandambharani@gmail.com>
2026-07-07 16:18:23 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,161 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(6+8)
+14
+a=4
+b=6
+print(a+b)
+10
+c=98
+print(c)
+98
+z=90
+print(z)
+90
+x=90
+print(x)
+90
+name="bharani"
+print(name)
+bharani
+city="vij"
+print(city)
+vij
+country="india"
+print(country)
+india
+
+aage=132
+aage=1322=67
+SyntaxError: cannot assign to literal
+a2=67
+print(a2)
+67
+4a=90
+SyntaxError: invalid decimal literal
+a01323456987
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    a01323456987
+NameError: name 'a01323456987' is not defined
+a0123456789=45
+print(a0123456789)
+45
+@=78
+SyntaxError: invalid syntax
+$=90
+SyntaxError: invalid syntax
+_=67
+print(_)
+67
+_d=27
+print(_d)
+27
+age=20
+name="bharani"
+print(age+" " +name)
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    print(age+" " +name)
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+age="20"
+name="bharani"
+print(age+" " +name)
+20 bharani
+if=90
+SyntaxError: invalid syntax
+first name="bharani"
+SyntaxError: invalid syntax
+first_name="bharani"
+print(first_name)
+bharani
+a=4,b=98
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=4;b=09
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+a=4;b=98
+print(a,b)
+4 98
+print(ab)
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    print(ab)
+NameError: name 'ab' is not defined. Did you mean: 'a'?
+print('a''b')
+ab
+a=234567
+print(a)
+234567
+a,b,c=345
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    a,b,c=345
+TypeError: cannot unpack non-iterable int object
+a,b,c=3,4,5
+print(a,b,c)
+3 4 5
+a,b,c=5668999
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    a,b,c=5668999
+TypeError: cannot unpack non-iterable int object
+a,b,c=(6,7,8)
+print(a,b,c)
+6 7 8
+abc=(6,7,8)
+print(abc)
+(6, 7, 8)
+a=5
+print(a)
+5
+del a
+print(a)
+Traceback (most recent call last):
+  File "<pyshell#61>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
+print(a2)
+67
+fname="bharani"
+lnmae="Nandam"
+print(fname+lname)
+Traceback (most recent call last):
+  File "<pyshell#65>", line 1, in <module>
+    print(fname+lname)
+NameError: name 'lname' is not defined. Did you mean: 'name'?
+fname="bharani"
+lname="Nandam"
+print(fname+lname)
+bharaniNandam
+print(fname+ lname)
+bharaniNandam
+print(fname+" " lname)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(fname+" "+lname)
+bharani Nandam
+>>> name="bharani"
+>>> print(name)
+bharani
+>>> NAME=NANDAMBHARANI
+Traceback (most recent call last):
+  File "<pyshell#74>", line 1, in <module>
+    NAME=NANDAMBHARANI
+NameError: name 'NANDAMBHARANI' is not defined
+>>> NAME="NANDAMBHARANI"
+>>> PRINT(NAME)
+Traceback (most recent call last):
+  File "<pyshell#76>", line 1, in <module>
+    PRINT(NAME)
+NameError: name 'PRINT' is not defined
+>>> NAME="NANDAMBHARANI"
+>>> print(NAME)
+NANDAMBHARANI
+>>> print(Name)
+Traceback (most recent call last):
+  File "<pyshell#79>", line 1, in <module>
+    print(Name)
+NameError: name 'Name' is not defined. Did you mean: 'name'?
+>>> Name="bharani"
+>>> print(Name)
+bharani
