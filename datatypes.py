c526a795ad34ef41830983bdc264ad846399f734
Nandam Bharani <nandambharani@gmail.com>
2026-07-08 14:38:10 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#datatypes
+a=7
+type(a)
+<class 'int'>
+b=7.8
+type(b)
+<class 'float'>
+>>> c='code'
+>>> print(c)
+code
+>>> type(c)
+<class 'str'>
+>>> e="python"
+>>> type(e)
+<class 'str'>
+>>> a=5+7j
+>>> type(a)
+<class 'complex'>
+>>> b=8j+7
+>>> type(b)
+<class 'complex'>
+>>> 6j
+6j
+>>> a=True
+>>> type(a)
+<class 'bool'>
+>>> b=False
+>>> type(b)
+<class 'bool'>
+>>> c=2j
+>>> type(c)
+<class 'complex'>
